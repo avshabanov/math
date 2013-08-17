7c8a08e25ad1741cbdd43a54f89fb5e70fc31c7e
Alexander Shabanov <avshabanov@gmail.com>
2013-08-18 02:30:23 +0400

    Adding optional prime upper bound

--- zeta/partial-factorization.lisp
+++ zeta/partial-factorization.lisp
@@ -5,9 +5,10 @@
 ;; Depending on primes in the same dir
 #+repl (load "primes.lisp")
 
-(defun produce-partial-factorizations (limit)
+(defun produce-partial-factorizations (limit &key (prime-upper-bound -1))
   (declare (type integer limit))
-  (let* ((primes (produce-primes 1 (+ (floor (sqrt limit)) 1)))
+  (let* ((max-after-prime (+ (if (< prime-upper-bound 0) limit prime-upper-bound) 1))
+	 (primes (produce-primes 1 max-after-prime))
 	 (last-prime-index (- (length primes) 1))
 	 (productions (make-array (+ limit 1) :element-type 'bit))
 	 (liouville-sum 1))
@@ -28,16 +29,27 @@
 	   (productions-for-prime i (aref primes i) t)))
     (values
      (loop for i from 0 to limit
-	 when (= 1 (aref productions i)) collect i)
+	when (= 1 (aref productions i)) collect i)
      liouville-sum)))
 
-#+repl (produce-partial-factorizations 9)
-#+repl (produce-partial-factorizations 25)
-#+repl (produce-partial-factorizations 49)
+#+repl (produce-partial-factorizations 9 :prime-upper-bound 3)
+#+repl (produce-partial-factorizations 25 :prime-upper-bound 5)
+#+repl (produce-partial-factorizations 49 :prime-upper-bound 7)
 
-#+repl (loop for i from 2 to 10000 do
-	    (multiple-value-bind (factors liouville-sum) (produce-partial-factorizations i)
+;; Sqrt upper bound
+#+repl (loop for i from 10000 to 10010 do
+	    (multiple-value-bind (factors liouville-sum)
+		(produce-partial-factorizations i
+						:prime-upper-bound (floor (sqrt i)))
 	      (declare (ignore factors))
-	      (format t "For n = ~S liouville sum = ~S~%" i liouville-sum)))
+	      (format t "[SQRT] For n = ~S liouville sum = ~S, lim = ~S~%"
+		      i liouville-sum (/ liouville-sum (sqrt i)))))
 
+;; Regular upper bound
+#+repl (loop for i from 10000 to 10010 do
+	    (multiple-value-bind (factors liouville-sum)
+		(produce-partial-factorizations i)
+	      (declare (ignore factors))
+	      (format t "[REG]  For n = ~S: liouville sum = ~S, lim = ~S~%"
+		      i liouville-sum (/ liouville-sum (sqrt i)))))
 
