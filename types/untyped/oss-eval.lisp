5a4b4cc6fda67f78c66f943d095cbf953e701fdc
Alexander Shabanov <avshabanov@gmail.com>
2013-08-29 22:45:54 +0400

    Adding oss eval proto

--- types/untyped/oss-eval.lisp
+++ types/untyped/oss-eval.lisp
@@ -1,52 +1,56 @@
 ;;
 ;; Illustrates evaluation for the small-step operation semantics
 
+(defclass grammar ()
+  ((value-map :initform (make-hash-table) :reader value-map)
+   (rule-map :initform (make-hash-table) :reader rule-map)))
+
+;; Main grammar creation function
+(defun make-grammar (&key values rules)
+  (let* ((g (make-instance 'grammar))
+	 (vmap (value-map g))
+	 (rmap (rule-map g)))
+    ;; init value mapping
+    (loop for v in values do
+	 (check-type v (or number symbol))
+	 (unless (null (gethash v vmap)) (error "Duplicate value ~S" v))
+	 (setf (gethash v vmap) v))
+    ;; init rules mapping
+    (loop for rule in rules do
+	 (check-type rule cons)
+	 (destructuring-bind (rfn pattern product) rule
+	   (cond
+	     ((eq '-> rfn)
+	      (check-type pattern cons)
+	      (check-type product keyword))
+	     (t (error "Unrecognized rule ~S" rule)))))
+    g))
+
+#+repl (defparameter *g-a-1* (make-grammar
+			      :values '(true false)
+			      :rules '((-> (if true :then :else) :then)
+				       (-> (if false :then :else) :else))))
+
 
 ;; Generic evaluator
-(defun oss-eval (expr semantics)
-  (declare (ignore expr))
-  (let ((rules (rest (second semantics))))
-    (loop for (ignored rule product) in rules do
-	 (labels ((eval-rule (rule product term-map)
-		    (unless (= (length rule) (length product))
-		      (return-from eval-rule nil))
-		    (loop for rule-form in rule
-		       for expr-term in expr
-		       do
-			 (cond
-			   ((null rule-form)
-			    (error "Rule form can not hold null values"))
-			   ((null expr-form) (return nil)) ; Length mismatch
-			   ((keywordp rule-form)
-			    (let ((prev (gethash rule-form term-map)))
-			      (unless (null prev)
-				(error "Term mentioned twice: illegal form???"))
-			      (setf (gethash rule-form term-map) expr-term)))
-			   ((or (numberp rule-form) (symbolp rule-form))
-			    (eq 'rule-form 'expr-term))
-			   ((consp rule-form) (error "Not implemented"))
-			   (t (format t "No match for rule ~S when matching the term ~S against ~S" rule expr-term rule-term)))))))
-	 (format t "~S -> ~S~%" rule product))))
+(defun toplev-eval (expr g)
+  (check-type g grammar)
+  (cond
+    ((keywordp expr) (error "Keyword values forbidden, got ~S" expr))
+    ((or (numberp expr) (symbolp expr))
+     (let ((v (gethash expr (value-map g))))
+       (if v v (error "Non-value expression ~S that pretend to be a value" expr))))
+    ((consp expr)
+     (destructuring-bind (fn-id &rest args) expr
+       (error "TODO: impl")))
+    (t (error "Invalid expression ~S" expr))))
 
 
 ;;
-;; REPL tests
+;; REPL
 
-#+repl (defparameter *s1*
-	 '(
-	   (:terms
-	    true
-	    false
-	    (if :t :t :t)
-	    0
-	    (succ :t)
-	    (pred :t)
-	    (iszero :t))
-	   (:rules
-	    (-> (if true :t2 :t3) :t2)
-	    (-> (if false :t2 :t3) :t3)
-	    (-> (pred (succ :t)) :t)
-	    (-> (pred 0) 0)
-	    (-> (iszero 0) true))))
-
-#+repl (oss-eval 'true *s1*)
+#+repl (let ((g (make-grammar :values '(true false)
+			      :rules '((-> (if true :then :else) :then)
+				       (-> (if false :then :else) :else)))))
+	 (values
+	  (toplev-eval 'true g)))
