90540d6e4ec9d4855577af6ac671c3162a78edf4
Alexander Shabanov <avshabanov@gmail.com>
2015-09-20 20:48:21 -0700

    Adding NonRecursiveFindNextExample

---
+++ discrete/trees/findNext/NonRecursiveFindNextExample.java
@@ -0,0 +1,83 @@
+import support.SimpleTreeSupport;
+
+
+/**
+ * Illustration of non-recursive implementation of 'find-next' algorithm in binary tree without using recursion and
+ * any other helper data structures.
+ *
+ * Sample output:
+ *
+ * <code>
+ * tree=
+ *     20
+ *   30
+ *     40
+ * 50
+ *     60
+ *   70
+ *     80
+ *
+ * 	value greater than=15 is 20
+ * 	value greater than=25 is 30
+ * 	value greater than=35 is 40
+ * 	value greater than=45 is 50
+ * 	value greater than=55 is 60
+ * 	value greater than=65 is 70
+ * 	value greater than=75 is 80
+ * Disconnected from the target VM, address: '127.0.0.1:53820', transport: 'socket'
+ * 	value greater than=85 is (none)
+ * tree=
+ * 50
+ *
+ * 	value greater than=40 is 50
+ * 	value greater than=50 is (none)
+ * 	value greater than=60 is (none)
+ * </code>
+ *
+ * @author Alexander Shabanov
+ */
+public final class NonRecursiveFindNextExample extends SimpleTreeSupport {
+
+  public static void main(String[] args) {
+    demo(new int[] {50, 30, 70, 20, 40, 60, 80}, new int[] {15, 25, 35, 45, 55, 65, 75, 85});
+    demo(new int[] {50}, new int[] {40, 50, 60});
+  }
+
+  //
+  // Private
+  //
+
+  private static void demo(int[] treeValues, int[] valuesGreaterThan) {
+    final Node tree = treeFromArray(treeValues);
+    System.out.println("tree=\n" + asString(tree));
+    for (final int value : valuesGreaterThan) {
+      final Node node = findNodeWithValueGreaterThan(tree, value);
+      System.out.println("\tvalue greater than=" + value + " is " + (node == null ? "(none)" : node.getValue()));
+    }
+  }
+
+  private static Node findNodeWithValueGreaterThan(Node node, int element) {
+    Node candidate = null;
+    for (;;) {
+      for (;;) {
+        if (node == null) {
+          break;
+        }
+
+        if (element >= node.getValue()) {
+          node = node.getRight(); // go to the right node until we meet a node which will be greater
+        } else {
+          break;
+        }
+      }
+
+      if (node == null) {
+        return candidate;
+      } else {
+        // element is strictly less than the given one - try to find something in the left subtree
+        candidate = node;
+        node = node.getLeft();
+      }
+    }
+  }
+}
--- discrete/trees/simpleTrees/support/SimpleTreeSupport.java
+++ discrete/trees/simpleTrees/support/SimpleTreeSupport.java
@@ -1,5 +1,7 @@
 package support;
 
+import java.util.Arrays;
+
 /**
  * Provides simple tree classes as well as some basic printing support.
  *
@@ -7,38 +9,69 @@ package support;
  */
 public abstract class SimpleTreeSupport {
 
-  protected static class Node {
-    final int value;
-    final Node left;
-    final Node right;
+  protected interface INode<N extends INode> {
+    int getValue();
+    N getLeft();
+    N getRight();
+    void setLeft(N left);
+    void setRight(N left);
+  }
 
-    public Node(int value, Node left, Node right) {
+  protected static abstract class AbstractNode<N extends INode> implements INode<N> {
+    private final int value;
+    private N left;
+    private N right;
+
+    protected AbstractNode(int value, N left, N right) {
       this.value = value;
       this.left = left;
       this.right = right;
     }
 
+    @Override
     public int getValue() {
       return value;
     }
 
-    public Node getLeft() {
+    @Override
+    public N getLeft() {
       return left;
     }
 
-    public Node getRight() {
+    @Override
+    public N getRight() {
       return right;
     }
 
+    @Override
+    public void setLeft(N left) {
+      this.left = left;
+    }
+
+    @Override
+    public void setRight(N right) {
+      this.right = right;
+    }
+
     public String toString() {
-      return "(v: " + value +
-          (left != null ? ", left: " + left : "") +
-          (right != null ? ", right: " + right : "") +
-          ')';
+      return asSinglelineString(this);
     }
   }
 
-  protected static String asString(Node node) {
+  protected static final class Node extends AbstractNode<Node> {
+    public Node(int value, Node left, Node right) {
+      super(value, left, right);
+    }
+  }
+
+  protected static String asSinglelineString(INode<?> node) {
+    return "(v: " + node.getValue() +
+        (node.getLeft() != null ? ", left: " + asSinglelineString(node.getLeft()) : "") +
+        (node.getRight() != null ? ", right: " + asSinglelineString(node.getRight()) : "") +
+        ')';
+  }
+
+  protected static String asString(INode<?> node) {
     final StringBuilder result = new StringBuilder(200);
     printNode(0, node, result);
     return result.toString();
@@ -46,24 +79,58 @@ public abstract class SimpleTreeSupport {
 
   // stringify helper
 
-  protected static void printNode(int indent, Node node, StringBuilder builder) {
+  protected static void printNode(int indent, INode<?> node, StringBuilder builder) {
     if (node == null) {
       return;
     }
 
-    printNode(indent + 2, node.left, builder);
+    printNode(indent + 2, node.getLeft(), builder);
 
     for (int i = 0; i < indent; ++i) {
       builder.append(' ');
     }
-    builder.append(node.value).append('\n');
+    builder.append(node.getValue()).append('\n');
 
-    printNode(indent + 2, node.right, builder);
+    printNode(indent + 2, node.getRight(), builder);
   }
 
   //
   // Tree structure
   //
+
+  protected static Node treeFromArray(int... values) {
+    Node root = null;
+    for (final int value : values) {
+      if (root == null) {
+        root = new Node(value, null, null);
+      } else {
+        // lookup for insertion place and put this value
+        Node node = root;
+        for (;;) {
+          if (value < node.getValue()) {
+            // goto left subtree
+            if (node.getLeft() != null) {
+              node = node.getLeft();
+            } else {
+              node.setLeft(new Node(value, null, null)); // parent=node
+              break;
+            }
+          } else if (value > node.getValue()) {
+            // goto right subtree
+            if (node.getRight() != null) {
+              node = node.getRight();
+            } else {
+              node.setRight(new Node(value, null, null)); // parent=node
+              break;
+            }
+          } else {
+            throw new IllegalArgumentException("Duplicate value=" + value + " in array=" + Arrays.toString(values));
+          }
+        }
+      }
+    }
+    return root;
+  }
 
   protected static Node n(int value, Node left, Node right) {
     return new Node(value, left, right);
