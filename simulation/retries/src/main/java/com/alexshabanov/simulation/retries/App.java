a1e195102c1a77c147db3c1a4996919426222934
Alexander Shabanov <avshabanov@gmail.com>
2015-10-02 00:56:04 -0700

    (Retry Simulation) Adding immediate retry client

--- simulation/retries/src/main/java/com/alexshabanov/simulation/retries/App.java
+++ simulation/retries/src/main/java/com/alexshabanov/simulation/retries/App.java
@@ -1,10 +1,92 @@
 package com.alexshabanov.simulation.retries;
 
+import com.alexshabanov.simulation.retries.logic.Client;
+import com.alexshabanov.simulation.retries.logic.Reporter;
+import com.alexshabanov.simulation.retries.logic.Server;
+import com.alexshabanov.simulation.retries.logic.support.ImmediateRetryClient;
+import com.alexshabanov.simulation.retries.logic.support.LinearBackoffClient;
+import com.alexshabanov.simulation.retries.logic.support.ServerImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Entry point.
  */
-public final class App {
-  public static void main(String[] args) {
+public final class App implements Runnable, Reporter {
 
+  private int succeededRequests = 0;
+  private int succeededRequestsTime = 0;
+
+  private int failedRequests = 0;
+  private int failedRequestsTime = 0;
+
+  public static void main(String[] args) {
+    new App().run();
+  }
+
+  @Override
+  public void reportRequest(int processTicks, boolean succeeded) {
+    if (succeeded) {
+      ++succeededRequests;
+      succeededRequestsTime += processTicks;
+    } else {
+      ++failedRequests;
+      failedRequestsTime += processTicks;
+    }
+  }
+
+  @Override
+  public void run() {
+    ClientFactory clientFactory;
+
+    System.out.println("== Immediate Retry ==");
+    clientFactory = ImmediateRetryClient::new;
+    runSimulation(5, 5, 100000, clientFactory); // 1:1
+    runSimulation(10, 5, 100000, clientFactory); // 2x overload
+    runSimulation(20, 5, 100000, clientFactory); // 4x overload
+
+    System.out.println("== Linear Backoff Retry ==");
+    clientFactory = LinearBackoffClient::new;
+    runSimulation(5, 5, 100000, clientFactory); // 1:1
+    runSimulation(10, 5, 100000, clientFactory); // 2x overload
+    runSimulation(20, 5, 100000, clientFactory); // 4x overload
+
+    System.out.println("===");
+  }
+
+  private interface ClientFactory {
+    Client create(int taskId, Reporter reporter);
+  }
+
+  private void runSimulation(int clientCount, int maxServerTaskCount, int maxTicks, ClientFactory factory) {
+    succeededRequests = 0;
+    succeededRequestsTime = 0;
+    failedRequests = 0;
+    failedRequestsTime = 0;
+
+    // init clients
+    final List<Client> clients = new ArrayList<>();
+    for (int i = 0; i < clientCount; ++i) {
+      clients.add(factory.create(i, this));
+    }
+
+    // run loops
+    final Server server = new ServerImpl(maxServerTaskCount, 50);
+    for (int i = 0; i < maxTicks; ++i) {
+      for (final Client client : clients) {
+        client.interact(server);
+      }
+      server.tick();
+    }
+
+    // calc statistics
+    System.out.println("clientCount=" + clientCount + ", maxServerTaskCount=" + maxServerTaskCount +
+        ", maxTicks=" + maxTicks);
+    System.out.println("\tSucceeded requests=" + succeededRequests +
+        " p100 time=" + (succeededRequests > 0 ? (succeededRequestsTime * 1.0 / succeededRequests) : 0) +
+        " Failed requests=" + failedRequests +
+        " p100 time=" + (failedRequests > 0 ? (failedRequestsTime * 1.0 / failedRequests) : 0) +
+        "");
   }
 }
--- simulation/retries/src/main/java/com/alexshabanov/simulation/retries/logic/support/LinearBackoffClient.java
+++ simulation/retries/src/main/java/com/alexshabanov/simulation/retries/logic/support/LinearBackoffClient.java
@@ -1,8 +1,8 @@
 package com.alexshabanov.simulation.retries.logic.support;
 
-import com.alexshabanov.simulation.retries.logic.Client;
-import com.alexshabanov.simulation.retries.logic.Reporter;
-import com.alexshabanov.simulation.retries.logic.Server;
+import com.alexshabanov.simulation.retries.logic.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Alexander Shabanov
@@ -10,11 +10,17 @@ import com.alexshabanov.simulation.retries.logic.Server;
 public final class LinearBackoffClient implements Client {
   private final Reporter reporter;
   private final int taskId;
+  private Mode mode = Mode.NEW;
+
   public int waitTime = 0;
   public int lastRetryTime = 0;
-  public int startRetryTime = 1;
-  public double multiplier = 2.0;
-  public int maxRetryTime = 5;
+
+  public int totalWaitTime = 0;
+  public int initRetryTime = 10;
+  public double multiplier = 1.5;
+  public int maxRetryTime = 200;
+  public double randMultiplier = 0.1;
+  public boolean reportEnabled = false;
 
   public LinearBackoffClient(int taskId, Reporter reporter) {
     this.reporter = reporter;
@@ -23,6 +29,61 @@ public final class LinearBackoffClient implements Client {
 
   @Override
   public void interact(Server server) {
+    ++totalWaitTime;
 
+    switch (mode) {
+      case NEW:
+        if (server.enqueue(new Request(taskId))) {
+          mode = Mode.POLL;
+        } else {
+          waitTime = getNextWaitTime();
+          if (waitTime < maxRetryTime) {
+            lastRetryTime = waitTime;
+            mode = Mode.BACKOFF;
+          } else {
+            reporter.reportRequest(totalWaitTime, false);
+            totalWaitTime = 0;
+            lastRetryTime = 0;
+            mode = Mode.NEW;
+          }
+        }
+        break;
+
+      case POLL:
+        final Response response = server.poll(taskId);
+        if (response != null) {
+          if (reportEnabled) {
+            System.out.println("Processed request for task #" + taskId);
+          }
+          mode = Mode.NEW;
+          reporter.reportRequest(totalWaitTime, true);
+          totalWaitTime = 0;
+        }
+        break;
+
+      case BACKOFF:
+        if (--waitTime < 0) {
+          mode = Mode.NEW;
+        }
+        break;
+
+      default:
+        throw new RuntimeException("Unknown mode=" + mode);
+    }
+  }
+
+  //
+  // Private
+  //
+
+  private int getNextWaitTime() {
+    final int time = (lastRetryTime != 0 ? (int) (lastRetryTime * multiplier) : initRetryTime);
+    return time + ThreadLocalRandom.current().nextInt((int) (time * randMultiplier));
+  }
+
+  private enum Mode {
+    NEW,
+    POLL,
+    BACKOFF
   }
 }
