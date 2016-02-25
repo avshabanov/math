b16d2bd8d28db71b17a4a423ce2a723701ec1114
Alexander Shabanov <avshabanov@gmail.com>
2016-02-24 22:45:41 -0800

    Adding optimized version of array diff demo

--- discrete/arrays/diffArray/ArrayDiffExample.java
+++ discrete/arrays/diffArray/ArrayDiffExample.java
@@ -9,13 +9,20 @@ import java.util.List;
 public final class ArrayDiffExample {
 
   public static void main(String[] args) {
+    demo(Arrays.asList(1, 2, 3, 5, 6, 8, 9, 11, 12, 13), 10);
     demo(Arrays.asList(1, 2, 3, 5, 6, 8, 9, 11, 12, 13), 3);
+    demo(Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15, 17, 19), 2);
+    demo(Arrays.asList(10, 15, 20), 7);
   }
 
   private static void demo(List<Integer> arr, int k) {
     Collections.sort(arr);
 
     final List<Pair> pairs = nonOptimizedGetDiff(arr, k);
+    final List<Pair> optPairs = optimizedGetDiff(arr, k);
+    if (!pairs.equals(optPairs)) {
+      throw new AssertionError("Error for arr=" + arr);
+    }
     System.out.println("arr=" + arr + ", k=" + k + ", pairs=" + pairs);
   }
 
@@ -27,6 +34,27 @@ public final class ArrayDiffExample {
         final int diff = arr.get(j) - arr.get(i);
         if (diff == k) {
           result.add(new Pair(arr.get(i), arr.get(j)));
+        } else if (diff > k) {
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  private static List<Pair> optimizedGetDiff(List<Integer> arr, int k) {
+    final List<Pair> result = new ArrayList<>();
+
+    int prevIndex = -1;
+    for (int i = 0; i < arr.size(); ++i) {
+      for (int j = Math.max(prevIndex, i + 1); j < arr.size(); ++j) {
+        final int diff = arr.get(j) - arr.get(i);
+        if (diff == k) {
+          result.add(new Pair(arr.get(i), arr.get(j)));
+        } else if (diff > k) {
+          prevIndex = j;
+          break;
         }
       }
     }
@@ -41,6 +69,25 @@ public final class ArrayDiffExample {
     public Pair(int first, int second) {
       this.first = first;
       this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Pair)) return false;
+
+      Pair pair = (Pair) o;
+
+      if (first != pair.first) return false;
+      return second == pair.second;
+
+    }
+
+    @Override
+    public int hashCode() {
+      int result = first;
+      result = 31 * result + second;
+      return result;
     }
 
     @Override
