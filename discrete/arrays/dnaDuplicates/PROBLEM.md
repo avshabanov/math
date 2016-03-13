5c39b8c59bb719db861d8dee4a39a07405f30f35
Alexander Shabanov <avshabanov@gmail.com>
2016-03-12 22:04:46 -0800

    Adding dna duplicates problem

---
+++ discrete/arrays/dnaDuplicates/PROBLEM.md
@@ -0,0 +1,2 @@
+Given a DNA sequence, e.g. ``AAAGTAAGTAAGTGGG....``.
+Find all the duplicates with length 10.
--- discrete/trees/minimumWeight/MinimumWeightExample.java
+++ discrete/trees/minimumWeight/MinimumWeightExample.java
@@ -31,6 +31,7 @@ import java.util.List;
  *     10
  * </pre>
  *
+ * @author Alexander Shabanov
  */
 public final class MinimumWeightExample extends SimpleTreeSupport {
 
