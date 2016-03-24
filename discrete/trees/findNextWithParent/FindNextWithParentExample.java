a4a275cfc69e0a536b15131a1d1d603780934f87
Alexander Shabanov <avshabanov@gmail.com>
2016-03-23 21:27:08 -0700

    Adding more test for 'find next' problem

--- discrete/trees/findNextWithParent/FindNextWithParentExample.java
+++ discrete/trees/findNextWithParent/FindNextWithParentExample.java
@@ -1,5 +1,11 @@
 import support.SimpleTreeWithParentSupport;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Another example of finding next element (parent link is actually unused here).
  *
@@ -18,6 +24,26 @@ public final class FindNextWithParentExample extends SimpleTreeWithParentSupport
                             n(100, n(90), n(110)),
                             n(140, n(130), n(150)))),
             5, 15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125, 135, 145, 155);
+
+    final Random random = new Random(4256091827395823127L);
+    for (int times = 0; times < 3; ++times) { // repeat test N times
+      // init tree
+      final int count = 100;
+      final List<Integer> values = IntStream.range(0, count).map((x) -> 2 * x + 1).boxed().collect(Collectors.toList());
+      Collections.shuffle(values, random);
+      final Node tree = treeFromArray(values.stream().mapToInt(x -> x).toArray());
+      for (int lookupValueIndex = 0; lookupValueIndex < count; ++lookupValueIndex) {
+        final int lookupValue = lookupValueIndex * 2;
+        final Node next = findNodeWithValueGreaterThan(tree, lookupValue);
+        if ((next == null) || (next.getValue() != (lookupValue + 1))) {
+          throw new AssertionError("Mismatch for lookup value=" + lookupValue);
+        }
+      }
+      if (null != findNodeWithValueGreaterThan(tree, (count + 1) * 2)) {
+        throw new AssertionError("Mismatch for max lookup value");
+      }
+    }
+    System.out.println("findGreaterNode - all tests succeeded");
   }
 
   private static void demo(String treeName, Node node, int... values) {
---
+++ discrete/trees/treeSuperPrinter/PROBLEM.md
@@ -0,0 +1,8 @@
+Write an algorithm to print tree in the console, so that every node connected with the others with arrows:
+
+```
+   .--- 5 ---.
+.- 2 -.   .- 7 -.
+3     4   6     8
+```
+
