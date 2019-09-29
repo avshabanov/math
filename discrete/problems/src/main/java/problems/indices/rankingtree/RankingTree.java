1f3d2d5cb3e4c046a616df741f8745b1021e17bf
Alexander Shabanov <avshabanov@gmail.com>
2019-09-29 14:06:18 -0700

    Fix ranking tree interface comment

--- discrete/problems/src/main/java/problems/indices/rankingtree/RankingTree.java
+++ discrete/problems/src/main/java/problems/indices/rankingtree/RankingTree.java
@@ -2,7 +2,8 @@ package problems.indices.rankingtree;
 
 /**
  * Ranking map is a map-alike data structure that supports dense ordering (ranking) for its elements based upon
- * comparison operation, so that the greatest element has ranking of 0, next one is ranked 1, then 2 and so forth.
+ * comparison operation, so that the smallest element has ranking of 0, element next to smallest is ranked 1,
+ * then 2 and so forth.
  */
 public interface RankingTree<K extends Comparable<K>, V> {
 
