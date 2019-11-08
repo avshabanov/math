86eb6921506785da056f79bbd2ca46021960ea8a
Alexander Shabanov <avshabanov@gmail.com>
2019-11-07 20:25:49 -0800

    groups: add README.md; add more tests

---
+++ groups/README.md
@@ -0,0 +1,8 @@
+# Experiments in Group Theory
+
+## TODO
+
+* Support infinite groups with identity element plus producer function
+* Add heuristics to verify group axioms for derived or produced groups
+* Lie groups
+* Detecting [automorphism](https://en.wikipedia.org/wiki/Automorphism)
--- groups/src/test/java/com/alexshabanov/groups/finite/FiniteGroupsTest.java
+++ groups/src/test/java/com/alexshabanov/groups/finite/FiniteGroupsTest.java
@@ -2,10 +2,12 @@ package com.alexshabanov.groups.finite;
 
 import com.alexshabanov.groups.finite.cayley.CayleyGroup;
 import com.alexshabanov.groups.finite.numbers.BaseNumericGroup;
+import com.google.common.collect.ImmutableList;
 import lombok.NonNull;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -35,29 +37,76 @@ public final class FiniteGroupsTest {
   }
 
   @Test
+  public void shouldEnsureComplementOnesGroupIsValid() {
+    FiniteGroups.ensureValid(new ComplementOnesGroup());
+  }
+
+  @Test
   public void shouldFailClosureCheck() {
     try {
       FiniteGroups.ensureValid(new NaiveNumericGroup(3));
-      fail("Naive group should fail closure property");
+      fail("Naive group should fail closure check");
     } catch (NonGroupException e) {
       assertTrue(e.getMessage(), e.getMessage().contains("Closure violation"));
     }
   }
 
-  private static final class NaiveNumericGroup extends BaseNumericGroup {
+  @Test
+  public void shouldFailInverseElementCheck() {
+    try {
+      FiniteGroups.ensureValid(new NonInversableGroup(3));
+      fail("Naive group should fail inverse element check");
+    } catch (NonGroupException e) {
+      assertTrue(e.getMessage(), e.getMessage().contains("Inverse element violation"));
+    }
+  }
+
+  @Test
+  public void shouldFailInverseElementAllegianceCheck() {
+    try {
+      FiniteGroups.ensureValid(new ComplementOnesGroup() {
+        @Override public Integer getIdentity() { return 0; }
+      });
+      fail("Complement ones group should not contain zero");
+    } catch (NonGroupException e) {
+      assertTrue(e.getMessage(), e.getMessage().contains("Identity element 0 does not belong to a group"));
+    }
+  }
+
+  //
+  // Private
+  //
+
+  private static class NaiveNumericGroup extends BaseNumericGroup {
 
     NaiveNumericGroup(int to) {
       super(1, to);
     }
 
-    @Override
-    public Integer getIdentity() {
+    @Override public Integer getIdentity() {
       return 1;
     }
 
-    @Override
-    public Integer mul(@NonNull Integer left, @NonNull Integer right) {
+    @Override public Integer mul(@NonNull Integer left, @NonNull Integer right) {
       return left * right;
     }
+  }
+
+  private static final class NonInversableGroup extends NaiveNumericGroup {
+    NonInversableGroup(int to) {
+      super(to);
+    }
+
+    @Override public Integer mul(@NonNull Integer left, @NonNull Integer right) {
+      return left;
+    }
+  }
+
+  private static class ComplementOnesGroup implements FiniteGroup<Integer> {
+    @Override public Integer getIdentity() { return 1; }
+    @Override public @NonNull List<Integer> getElements() { return ImmutableList.of(-1, 1); }
+    @Override public Integer mul(@NonNull Integer left, @NonNull Integer right) { return left * right; }
+
+    @Override public String toString() { return "{-1, 1}"; }
   }
 }
